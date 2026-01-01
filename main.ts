ef88e8dc247dfb7b21cb40dc5321ba194fd09a08
kmurp62rulz <kmurp62rulz@gmail.com>
2026-01-01 23:13:19 +1000

    Initial commit: Deno hello world app

---
+++ main.ts
@@ -0,0 +1,8 @@
+function helloWorld() {
+  return "Hi Mum!"
+}
+
+// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
+if (import.meta.main) {
+  console.log(helloWorld());
+}
