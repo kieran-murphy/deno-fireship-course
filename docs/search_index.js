a7e4b409a2ed207c67b1e7793933a5c86e9f9536
kieran-murphy <68723269+kieran-murphy@users.noreply.github.com>
2026-01-03 23:28:25 +1100

    Documentation

---
+++ docs/search_index.js
@@ -0,0 +1,3 @@
+(function () {
+  window.DENO_DOC_SEARCH_INDEX = {"kind":"search","nodes":[{"id":"namespace_multiply","kind":[{"char":"f","kind":"Function","title":"Function"}],"name":"multiply","file":".","doc":"Multiplies two numbers and returns the result.","url":"././~/multiply.html","deprecated":false}]};
+})()
---
+++ lib.ts
@@ -0,0 +1,7 @@
+/**
+ * Multiplies two numbers and returns the result. 
+ */
+
+export function multiply(a: number, b: number) {
+    return a * b;
+}
