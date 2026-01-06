0bbb7bd4c9513d4c7d50bb42a3c4bf72ded410a7
kmurp62rulz <kmurp62rulz@gmail.com>
2026-01-06 23:30:10 +1000

    Multi-threading

--- main.ts
+++ main.ts
@@ -1,6 +1,18 @@
-import foo from "./a";
-import { bar, baz } from "./b";
+const numbers = [50, 50, 50, 50, 50]; 
 
-foo();
-bar();
-baz();
+numbers.forEach((n) => {
+  const worker = new Worker(
+    new URL("./worker.ts", import.meta.url).href,
+    {
+      type: "module",
+    }
+  );
+// 1a. send data to worker to start 
+  worker.postMessage({ n });
+
+ // 2b. Receive completed work from worker
+  worker.onmessage = (e) => {
+    console.log(`Main Thread (n=${n}):`, e.data);
+    worker.terminate();
+  };
+});
